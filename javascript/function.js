ec869d696c55fccc8c560998af05820d372dc24b
Alain Dimabuyo <cutelykamanalo11@yahoo.com>
2017-07-17 22:07:33 +0800

    Responsive-layout
    responsive settings for about me and work section

--- javascript/function.js
+++ javascript/function.js
@@ -1,3 +1,8 @@
+$(function() {
+	smoothScroll(300);
+});
+
+
 function smoothScroll (duration) {
  	      $('a[href^="#"]').on('click', function(event) {
 
@@ -10,4 +15,5 @@ function smoothScroll (duration) {
  	              }, duration);
  	            }
  	       });
+
 }
